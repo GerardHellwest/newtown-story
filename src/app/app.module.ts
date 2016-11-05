fd048e73226a7d6564718777fe3aa12318b4a8cd
Gerard Wilson <lucid7net@gmail.com>
2016-11-05 13:22:03 +1100

    adding firebase and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+// import * as firebase from 'firebase';
 import { AppComponent } from './app.component';
+
+
+let config = {
+  apiKey: 'AIzaSyDMzzDI25r1JBwbRgjO9RCvuvwS5rDUZTU',
+  authDomain: 'newtown-story.firebaseapp.com',
+  databaseURL: 'https://newtown-story.firebaseio.com',
+  storageBucket: 'newtown-story.appspot.com',
+  messagingSenderId: '915123734153'
+}
+
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +26,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
